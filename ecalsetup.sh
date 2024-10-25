9bf50363689c105e7601a69f73a7e82c65e8fe67
Raghul Raj <76444923+raghulrajn@users.noreply.github.com>
2024-10-25 12:17:12 +0200

    Update ecalsetup.sh

--- ecalsetup.sh
+++ ecalsetup.sh
@@ -12,6 +12,7 @@ sudo apt install python3-pip
 sudo add-apt-repository ppa:ecal/ecal-5.13
 sudo apt-get update
 sudo apt-get install ecal
+sudo apt install python3-ecal5
 sudo apt install libc6 libcurl4 libgcc-s1 libhdf5-103 libprotobuf23 libqt5core5a libqt5gui5 libqt5widgets5 libqt5svg5 libstdc++6 sysstat ifstat libqwt-qt5-6 libyaml-cpp0.7
 
 sudo apt-get install protobuf-compiler

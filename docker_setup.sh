350a3defe111a1565f1c9f7d477f5c4686d8d209
Raghul Raj <76444923+raghulrajn@users.noreply.github.com>
2024-10-27 12:36:06 +0100

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+sudo apt-get update
+sudo apt-get -y install podman
 sudo apt install -y docker.io
 sudo systemctl enable docker
 sudo groupadd docker
 sudo usermod -aG docker $USER
 newgrp docker
+code --install-extension ms-vscode-remote.remote-containers
